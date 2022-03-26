79e1233323975a437e7254cbbaa6d1e97b654278
Mikey <Michael.parsons2323@gmail.com>
2022-03-26 13:48:04 -0700

    made rps ui

--- javascript.js
+++ javascript.js
@@ -1,88 +1,117 @@
+let playerScore = 0;
+let comScore = 0;
+
+document.getElementById("pscore").innerHTML = "Player Score: " + playerScore;
+document.getElementById("cscore").innerHTML = "Com Score: " + comScore;
+
+const rbutton = document.querySelector('#rbutton');
+rbutton.addEventListener('click', () => {
+    playerChoice = "ROCK";
+    comChoice = computerSelection();
+    playRound(playerChoice, comChoice);
+    updateScore();
+    gameOver();
+});
+const pbutton = document.querySelector('#pbutton');
+pbutton.addEventListener('click', () => {
+    playerChoice = "PAPER";
+    comChoice = computerSelection();
+    playRound(playerChoice, comChoice);
+    updateScore();
+    gameOver();
+});
+const sbutton = document.querySelector('#sbutton');
+sbutton.addEventListener('click', () => {
+    playerChoice = "SCISSORS";
+    comChoice = computerSelection();
+    playRound(playerChoice, comChoice);
+    updateScore();
+    gameOver();
+});
+
+
+
+const text = document.querySelector('.text');
+function playRound(playerChoice, computerSelection){
+    if (playerChoice == computerSelection) 
+    {
+        text.textContent = "Tie!";
+    }
+    if (playerChoice == "ROCK")
+    {
+        if (computerSelection == "PAPER") 
+        {
+            text.textContent = "You Lose!";
+            comScore++;
+        }
+        else if (computerSelection == "SCISSORS")
+        {
+            text.textContent = "You Win!";
+            playerScore++;
+        }
+    }
+    else if (playerChoice == "PAPER")
+    {
+        if (computerSelection == "ROCK") 
+        {
+            text.textContent = "You Win!";
+            playerScore++;
+        }
+        else if (computerSelection == "SCISSORS")
+        {
+            text.textContent = "You Lose!";
+            comScore++;
+        }
+    }
+    else if (playerChoice == "SCISSORS")
+    {
+        if (computerSelection == "ROCK") 
+        {
+            text.textContent = "You Lose!";
+            comScore++;
+        }
+        else if (computerSelection == "PAPER")
+        {
+            text.textContent = "You Win!";
+            playerScore++;
+        }
+    }
+
+}
+
 function computerSelection() {
-    let comChoice = (Math.floor(Math.random() * 3))
-    if (comChoice == 0)
+    let x = (Math.floor(Math.random() * 3))
+    if (x == 0)
     {
         return "ROCK";
     }
-    else if (comChoice == 1)
+    else if (x == 1)
     {
         return "PAPER";
     }
-    else if (comChoice == 2)
+    else if (x == 2)
     {
         return "SCISSORS";
     }
 }
 
-
-function playerSelection() {
-    let playerChoice = prompt("Enter Rock, Paper, or Scissors.");
-    return playerChoice.toUpperCase();
-
+function updateScore() {
+    document.getElementById("pscore").innerHTML = "Player Score: " + playerScore;
+    document.getElementById("cscore").innerHTML = "Com Score: " + comScore;
 }
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection) 
-    {
-        return "Tie!";
+function gameOver() {
+    if (playerScore == 5){
+        text.textContent = "Game Over, You Win!";
+        document.getElementById('rbutton').disabled = true;
+        document.getElementById('pbutton').disabled = true;
+        document.getElementById('sbutton').disabled = true;
     }
-    if (playerSelection == "ROCK")
-    {
-        if (computerSelection == "PAPER") 
-        {
-            return "You Lose!";
-            comScore++;
-        }
-        else if (computerSelection == "SCISSORS")
-        {
-            return "You Win!";
-            playerScore++;
-        }
-    }
-    else if (playerSelection == "PAPER")
-    {
-        if (computerSelection == "ROCK") 
-        {
-            return "You Win!";
-            playerScore++;
-        }
-        else if (computerSelection == "SCISSORS")
-        {
-            return "You Lose!";
-            comScore++;
-        }
-    }
-    else if (playerSelection == "SCISSORS")
-    {
-        if (computerSelection == "ROCK") 
-        {
-            return "You Lose!";
-            comScore++;
-        }
-        else if (computerSelection == "PAPER")
-        {
-            return "You Win!";
-            playerScore++;
-        }
+    else if (comScore == 5){
+        text.textContent = "Game Over, You Lose!";
+        document.getElementById('rbutton').disabled = true;
+        document.getElementById('pbutton').disabled = true;
+        document.getElementById('sbutton').disabled = true;
     }
 }
 
-function game() 
-{
-    let playerScore = 0;
-    let comScore = 0;
-    for (i = 0; i < 5; i++)
-    {
-        console.log(playRound(playerSelection(), computerSelection()));
-    }
-    if (playerScore > comScore)
-    {
-        console.log("You win the game!");
-    }
-    else 
-    {
-        console.log("You lose the game!");
-    }
-}
-
-game();
